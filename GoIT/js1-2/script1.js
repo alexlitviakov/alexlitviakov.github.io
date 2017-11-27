aa9b2111f3fa9937bd72f1f816eca82ab3021629
alexlitviakov <wmplaza@mail.ru>
2017-11-27 14:31:13 +0200

    Update script1.js

--- GoIT/js1-2/script1.js
+++ GoIT/js1-2/script1.js
@@ -1,20 +1,46 @@
-var base = prompt('Введите базовое число');
-var exp = prompt('Введите экспоненту');
-
-function pow(base, exp) {
-
-if (exp == 0) {
-
-return 1;
-} else if(exp < 0) {
-
-return 1 / base * pow(base, exp + 1);
-
+function pow(num, power) {
+    var result = num;
+    if (power > 0)
+    {
+        while (power > 1)
+        {
+            result = result * num;
+            power--;
+        } 
+        return result;
+    }
+    else if (power < 0)
+    {
+        power *= -1;
+        while (power > 1)
+        {
+            result = result * num;
+            power--;
+        } 
+        return 1/result;
+    }
+    else
+    {
+        return 1;
+    }
 }
-return base * pow(base, exp - 1);
 
+var promptNumber = prompt('Input number: ');
+var userNumber = parseInt(promptNumber);
+if (isNaN(userNumber))
+{
+    console.log('Number invalid!');
 }
-
-//console.log(pow(2, -3));
-
-console.log(pow(base, exp) );
+else 
+{
+    var promptPower = prompt('Input power: ');
+    var userPower = parseInt(promptPower);
+    if (isNaN(userPower))
+    {
+        console.log('Power is not a number!');
+    } 
+    else
+    {
+        console.log(pow(userNumber, userPower));
+    }   
+}
